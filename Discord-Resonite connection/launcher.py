5e82a129b5e8febe0b0ff19fcfe7c32a358c177c
David Deppeler <58292410+Ratbagdoo@users.noreply.github.com>
2024-02-03 17:17:51 +1100

    Update launcher.py

--- Discord-Resonite connection/launcher.py
+++ Discord-Resonite connection/launcher.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     subprocess.Popen(["python", "websoket_server_discord.py"])
     subprocess.Popen(["python", "discord_bot.py"])
     subprocess.Popen(["python", "Websocket_Server_Send"])
-    # Add subprocess for HTML server interface if needed
+    # Add subprocess.Popen for new flies if needed
